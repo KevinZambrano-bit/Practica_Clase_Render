5d18bf9f80772ef0fd6b263927167d68a9b1040b
KevinZambrano-bit <git config --global user.email tu-correo@ejemplo.com>
2026-06-03 20:43:34 -0500

    nombre commit

--- src/main.ts
+++ src/main.ts
@@ -34,7 +34,7 @@ async function bootstrap() {
 
   // ── Swagger ───────────────────────────────────────────────────
   const config = new DocumentBuilder()
-    .setTitle('StockMaster API')
+    .setTitle('Kevin Zambrano')
     .setDescription('Sistema de Gestión de Inventarios — Caso Práctico 3')
     .setVersion('1.0')
     .addBearerAuth(
